846cf609b3cfa5d0dda15ea488c25d61136c020c
DeadDeal7 <98024247+DeadDeal7@users.noreply.github.com>
2022-03-27 16:03:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ async def getmedia(bot, update):
         )
         return
     await text.edit_text(
-        text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>Join :-</b> @FayasNoushad",
+        text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>Join :-</b> @DeadDealBots 💛",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"), InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}"),],
                                            [InlineKeyboardButton(text="⚙ Join Updates Channel ⚙", url="https://telegram.me/DeadDealBots")]])
